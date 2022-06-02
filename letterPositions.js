e820c8b85130f10307a975e6a2409f6e2974c223
Zuge Gao <audreygaozu@hotmail.com>
2022-06-02 00:00:50 +0000

    letter position function

---
+++ letterPositions.js
@@ -0,0 +1,18 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  strings = sentence.replace(/\s+/g, '')
+  for(const letter of strings){
+    results[letter] = [];
+  }
+  for (let i = 0; i < sentence.length; i++) {
+    if(sentence[i] !== " "){
+      results[sentence[i]].push(i);
+    }
+      
+  }
+  console.log(results);
+  return results;
+};
+
+letterPositions("hello");
