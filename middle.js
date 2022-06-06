ae9614f3893c873f66634b46bec01f8ee3c17ec4
Zuge Gao <audreygaozu@hotmail.com>
2022-06-06 20:12:05 +0000

    deleted comments

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-// const eqArrays = function(sample1,sample2){
-//   if (sample1.length === sample2.length) {
-//     for (let i = 0; i < sample1.length; i++) {
-//       if(sample1[i] !== sample2[i]){
-//         return false;
-//       } 
-    
-//     }
-//     return true; 
-    
-//   } else {
-//     return false;
-//   }
-
-// }
-
-// const assertArraysEqual = function(sample1,sample2){
-//   if (eqArrays(sample1,sample2)) {
-//     console.log(`✅✅✅ two arrays are equal`);
-//   } else {
-//     console.log(`❌❌❌ two arrays are not equal`);
-//   }
-
-// }
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
