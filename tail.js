3ae9d7f0ae36650d5465d607ec4cbdd13e1ad227
Zuge Gao <audreygaozu@hotmail.com>
2022-06-06 19:55:21 +0000

    deleted comments

--- tail.js
+++ tail.js
@@ -1,15 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     //console.log("✅✅✅ Assertion Passed: " + actual + "===" + expected);
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     //console.log("❌❌❌ Assertion Failed: " + actual + "!==" + expected);
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-
-// };
-
-
 const tail = function(array) {
   let tail = array.slice(0);
   return tail;
